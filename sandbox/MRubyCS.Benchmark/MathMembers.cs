d4e3f0ad2c02bea95faaaca483db85a520b7f40d
Akeit0 <90429982+Akeit0@users.noreply.github.com>
2025-08-24 18:13:18 +0900

    add: bm_ao_render.rb

---
+++ sandbox/MRubyCS.Benchmark/MathMembers.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace MRubyCS.Benchmark;
+
+class MathMembers
+{
+    [MRubyMethod(RequiredArguments = 1)]
+    public static MRubyMethod Sin = new((state, self) =>
+    {
+        var arg = state.GetArgumentAt(0);
+        var b = arg.VType switch
+        {
+            MRubyVType.Float => arg.FloatValue,
+            _ => state.ToFloat(arg)
+        };
+        return MRubyValue.From(Math.Sin(b));
+    });
+
+    [MRubyMethod(RequiredArguments = 1)]
+    public static MRubyMethod Cos = new((state, self) =>
+    {
+        var arg = state.GetArgumentAt(0);
+        var b = arg.VType switch
+        {
+            MRubyVType.Float => arg.FloatValue,
+            _ => state.ToFloat(arg)
+        };
+        return MRubyValue.From(Math.Cos(b));
+    });
+
+    [MRubyMethod(RequiredArguments = 1)]
+    public static MRubyMethod Sqrt = new((state, self) =>
+    {
+        var arg = state.GetArgumentAt(0);
+        var b = arg.VType switch
+        {
+            MRubyVType.Float => arg.FloatValue,
+            _ => state.ToFloat(arg)
+        };
+        return MRubyValue.From(Math.Sqrt(b));
+    });
+}
