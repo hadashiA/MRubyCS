0fe37fa24ab9af79f2e55677cb09039f44ebc1e7
hadashiA <dev@hadashikick.jp>
2026-01-17 18:00:24 +0900

    Fix test for Random

--- src/MRubyCS/StdLib/RandomMembers.cs
+++ src/MRubyCS/StdLib/RandomMembers.cs
@@ -1,10 +1,11 @@
 using System;
+using System.Buffers;
 
 namespace MRubyCS.StdLib;
 
 class MRubyRandomData
 {
-    public Random Rand { get; private set; } = null!;
+    public Random Random { get; private set; } = null!;
     public int Seed => seed;
 
     int seed;
@@ -22,44 +23,81 @@ class MRubyRandomData
     public void SetSeed(int seed)
     {
         this.seed = seed;
-        Rand = new Random(seed);
+        Random = new Random(seed);
     }
 
     public void ResetSeed()
     {
         SetSeed(Guid.NewGuid().GetHashCode());
     }
-}
 
-public class RandomMembers
-{
-    public static MRubyMethod DefaultRand = new((mrb, self) =>
+    public MRubyValue Rand(MRubyState mrb)
     {
-        var rand = DefaultInstanceOf(mrb).Rand;
-        if (mrb.TryGetArgumentAt(0, out var maxValue))
+        if (mrb.TryGetArgumentAt(0, out var arg0))
         {
-            var max = mrb.AsInteger(maxValue);
-            return rand.Next(0, (int)max);
+            switch (arg0.VType)
+            {
+                case MRubyVType.Integer:
+                case MRubyVType.Float:
+                    var max = mrb.AsInteger(arg0);
+                    if (max <= 0)
+                    {
+                        return Random.NextDouble();
+                    }
+                    return Random.Next(0, (int)max);
+                case MRubyVType.Range:
+                    var range = arg0.As<RRange>();
+                    if (range.Begin.IsInteger && range.End.IsInteger)
+                    {
+                        return RandonRangeInt(
+                            (int)range.Begin.IntegerValue,
+                            (int)range.End.IntegerValue,
+                            range.Exclusive);
+                    }
+                    var begin = CastToFloat(range.Begin);
+                    var end = CastToFloat(range.End);
+                    return RandomRangeFloat(begin, end, range.Exclusive);
+            }
         }
-        return rand.NextDouble();
-    });
+        return Random.NextDouble();
 
-    public static MRubyMethod DefaultSRand = new((mrb, self) =>
+        MRubyValue RandonRangeInt(int begin, int end, bool exclusive)
+        {
+            if (end - begin <= 0) return default;
+            return Random.Next(begin, end + (exclusive ? 0 : 1));
+        }
+
+        MRubyValue RandomRangeFloat(double begin, double end, bool exclusive)
+        {
+            var length = end - begin + (exclusive ? 0 : 1);
+            if (length <= 0) return default;
+            return Random.NextDouble() * length + begin;
+        }
+
+        double CastToFloat(MRubyValue value)
+        {
+            if (value.IsFloat) return value.FloatValue;
+            if (value.IsInteger) return value.IntegerValue;
+            mrb.Raise(Names.TypeError, $"no implicit conversion of {value} into Integer");
+            return default; // unreached
+        }
+    }
+
+    public MRubyValue SRand(MRubyState mrb)
     {
-        var data =  DefaultInstanceOf(mrb);
-        var currentSeed = data.Seed;
+        var currentSeed = Seed;
         if (mrb.TryGetArgumentAt(0, out var seedValue))
         {
-            data.SetSeed((int)mrb.AsInteger(seedValue));
+            SetSeed((int)mrb.AsInteger(seedValue));
         }
         else
         {
-            data.ResetSeed();
+            ResetSeed();
         }
         return currentSeed;
-    });
+    }
 
-    public static MRubyMethod DefaultBytes = new((mrb, self) =>
+    public MRubyValue Bytes(MRubyState mrb)
     {
         var length = mrb.GetArgumentAsIntegerAt(0);
         if (length < 0)
@@ -67,10 +105,32 @@ public class RandomMembers
             mrb.Raise(Names.ArgumentError, "negative string size"u8);
         }
 
-        var rand =  DefaultInstanceOf(mrb).Rand;
+        if (length < 0)
+        {
+            mrb.Raise(Names.ArgumentError, "negative string size"u8);
+        }
+
         var bytes = new byte[length];
-        rand.NextBytes(bytes);
+        Random.NextBytes(bytes);
         return mrb.NewStringOwned(bytes);
+    }
+}
+
+static class RandomMembers
+{
+    public static MRubyMethod DefaultRand = new((mrb, self) =>
+    {
+        return DefaultInstanceOf(mrb).Rand(mrb);
+    });
+
+    public static MRubyMethod DefaultSRand = new((mrb, self) =>
+    {
+        return DefaultInstanceOf(mrb).SRand(mrb);
+    });
+
+    public static MRubyMethod DefaultBytes = new((mrb, self) =>
+    {
+        return DefaultInstanceOf(mrb).Bytes(mrb);
     });
 
     public static MRubyMethod Initialize = new((mrb, self) =>
@@ -90,42 +150,17 @@ public class RandomMembers
 
     public static MRubyMethod Rand = new((mrb, self) =>
     {
-        var rand = InstanceOf(self).Rand;
-        if (mrb.TryGetArgumentAt(0, out var maxValue))
-        {
-            var max = mrb.AsInteger(maxValue);
-            return rand.Next(0, (int)max);
-        }
-        return rand.NextDouble();
+        return InstanceOf(mrb, self).Rand(mrb);
     });
 
     public static MRubyMethod SRand = new((mrb, self) =>
     {
-        var data =  InstanceOf(self);
-        var currentSeed = data.Seed;
-        if (mrb.TryGetArgumentAt(0, out var seedValue))
-        {
-            data.SetSeed((int)mrb.AsInteger(seedValue));
-        }
-        else
-        {
-            data.ResetSeed();
-        }
-        return currentSeed;
+        return InstanceOf(mrb, self).SRand(mrb);
     });
 
     public static MRubyMethod Bytes = new((mrb, self) =>
     {
-        var length = mrb.GetArgumentAsIntegerAt(0);
-        if (length < 0)
-        {
-            mrb.Raise(Names.ArgumentError, "negative string size"u8);
-        }
-
-        var rand =  InstanceOf(self).Rand;
-        var bytes = new byte[length];
-        rand.NextBytes(bytes);
-        return mrb.NewStringOwned(bytes);
+        return InstanceOf(mrb, self).Bytes(mrb);
     });
 
     public static MRubyMethod ArrayShuffle = new((mrb, self) =>
@@ -143,7 +178,16 @@ public class RandomMembers
         {
             return self;
         }
-        var rand =  DefaultInstanceOf(mrb).Rand;
+
+        Random rand;
+        if (mrb.TryGetKeywordArgument(mrb.Intern("random"u8), out var randomInstance))
+        {
+            rand = InstanceOf(mrb, randomInstance).Random;
+        }
+        else
+        {
+            rand = DefaultInstanceOf(mrb).Random;
+        }
         for (var i = array.Length - 1; i > 0; i--)
         {
             var j = rand.Next(0, i + 1);
@@ -154,21 +198,92 @@ public class RandomMembers
 
     public static MRubyMethod ArraySample = new((mrb, self) =>
     {
+        Random rand;
+        if (mrb.TryGetKeywordArgument(mrb.Intern("random"u8), out var randomInstance))
+        {
+            rand = InstanceOf(mrb, randomInstance).Random;
+        }
+        else
+        {
+            rand = DefaultInstanceOf(mrb).Random;
+        }
+
         var array = self.As<RArray>();
-        var rand =  DefaultInstanceOf(mrb).Rand;
-        var i = rand.Next(0, array.Length);
-        return array[i];
+        if (mrb.TryGetArgumentAt(0, out var givenValue))
+        {
+            var n = (int)mrb.AsInteger(givenValue);
+            // if (n > len) n = len;
+            if (n < 0)
+            {
+                mrb.Raise(Names.ArgumentError, "negative sample number"u8);
+            }
+
+            if (n > array.Length) n =  array.Length;
+
+            int[]? rentArray = null;
+            var indices = n <= 255
+                ? stackalloc int[n]
+                : rentArray = ArrayPool<int>.Shared.Rent(n);
+
+            for (var i = 0; i < n; i++)
+            {
+                Retry:
+                var index = rand.Next(0, n);
+                for (var j = 0; j < i; j++)
+                {
+                    if (indices[j] == index) goto Retry;
+                }
+                indices[i] = index;
+            }
+
+            var result = mrb.NewArray(n);
+            for (var i = 0; i < n; i++)
+            {
+                var index = indices[i];
+                result.Push(array[index]);
+            }
+
+            if (rentArray != null)
+            {
+                ArrayPool<int>.Shared.Return(rentArray);
+            }
+
+            return result;
+        }
+
+        switch (array.Length)
+        {
+            case 0:
+                return default;
+            case 1:
+                return array[0];
+            default:
+                var i = rand.Next(0, array.Length);
+                return array[i];
+        }
     });
 
     static MRubyRandomData DefaultInstanceOf(MRubyState mrb)
     {
         var randomClass = mrb.GetClass(mrb.Intern("Random"u8));
         var defaultValue = mrb.GetInstanceVariable(randomClass, Names.Default);
-        return InstanceOf(defaultValue);
+        return (defaultValue.As<RData>().Data as MRubyRandomData)!;
     }
 
-    static MRubyRandomData InstanceOf(MRubyValue value)
+    static MRubyRandomData InstanceOf(MRubyState mrb, MRubyValue value)
     {
-        return (value.As<RData>().Data as MRubyRandomData)!;
+        var randomClass = mrb.GetClass(mrb.Intern("Random"u8));
+        var obj = value.As<RObject>();
+        if (obj.Class != randomClass)
+        {
+            mrb.Raise(Names.TypeError, "Not a Random instance"u8);
+        }
+
+        if (obj is RData { Data: MRubyRandomData data })
+        {
+            return data;
+        }
+        mrb.Raise(Names.TypeError, "Not a Random instance"u8);
+        return default!; // unreached
     }
 }
